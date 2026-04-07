7ff2ca5bfdf77a1e9e3858916da8eed25b994328
adriaclavero2 <adriaclavero2@gmail.com>
2026-04-07 11:14:26 +0200

    feat: implement service layer and global exception handling for User and Event

--- src/main/java/services/EventService.java
+++ src/main/java/services/EventService.java
@@ -1,4 +1,13 @@
 package services;
 
+import models.Event;
+
+import java.util.List;
+
 public interface EventService {
+    Event saveEvent(Event event);
+
+    List<Event> getAllEvents();
+
+    Event getEventsById(Long id);
 }
---
+++ src/main/java/services/EventServiceImpl.java
@@ -0,0 +1,34 @@
+package services;
+
+import exceptions.ResourceNotFoundException;
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import models.Event;
+import models.User;
+import org.springframework.stereotype.Service;
+import repositories.EventRepository;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class EventServiceImpl implements EventService{
+
+    private final EventRepository eventRepository;
+
+   @Override
+   public Event saveEvent(Event event) {
+        return eventRepository.save(event);
+    }
+
+    @Override
+    public List<Event> getAllEvents() {
+        return eventRepository.findAll();
+    }
+
+    @Override
+    public Event getEventsById(Long id) {
+        return eventRepository.findAllById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("The event with ID was not found: " + id));
+    }
+}
